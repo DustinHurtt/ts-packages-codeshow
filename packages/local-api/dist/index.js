5dc03dbb3656fc33d9f77f7915f4747343c6dd5f
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-09-20 11:53:58 -0400

    added express server to local-api

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,5 +1,14 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => {
-    console.log('Server is listening');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serve = void 0;
+const express_1 = __importDefault(require("express"));
+const serve = (port, filename, dir) => {
+    const app = (0, express_1.default)();
+    app.listen(port, () => {
+        console.log('Listening on port', port);
+    });
+};
+exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,4 +1,10 @@
-export default () => {
-    console.log('Server is listening')
+import express from 'express';
+
+export const serve = (port: number, filename: string, dir: string) => {
+    const app = express();
+
+    app.listen(port, () => {
+        console.log('Listening on port', port)
+    });
 };
 
