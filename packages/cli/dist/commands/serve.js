a54555a0af4cbdfbecb8d48f7f8e3c1b9b1c4458
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-09-20 10:53:49 -0400

    adding commands to cli

---
+++ packages/cli/dist/commands/serve.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serveCommand = void 0;
+const commander_1 = require("commander");
+exports.serveCommand = new commander_1.Command()
+    .command('serve [filename]')
+    .description('Open a file for editing')
+    .option('-p, --port <number>', 'port to run server on', '4005')
+    .action((filename = 'notebook.js', options) => {
+    console.log(filename, options);
+});
---
+++ packages/cli/src/commands/serve.ts
@@ -0,0 +1,9 @@
+import { Command } from "commander";
+
+export const serveCommand = new Command()
+    .command('serve [filename]')
+    .description('Open a file for editing')
+    .option('-p, --port <number>', 'port to run server on', '4005')
+    .action((filename = 'notebook.js', options) => {
+        console.log(filename, options)
+    });
