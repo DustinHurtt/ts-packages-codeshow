725ce4ca632202ae807f142d7258bb8a6033f565
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-09-20 17:09:19 -0400

    having issue with cli command

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,12 +1,31 @@
-import path from 'path';
+import path from "path";
 import { Command } from "commander";
-import { serve } from 'local-api'
+import { serve } from "local-api";
+
+interface LocalApiError {
+  code: string;
+}
 
 export const serveCommand = new Command()
-    .command('serve [filename]')
-    .description('Open a file for editing')
-    .option('-p, --port <number>', 'port to run server on', '4005')
-    .action((filename = 'notebook.js', options: { port: string }) => {
-        const dir = path.join(process.cwd(), path.dirname(filename));
-        serve(parseInt(options.port), path.basename(filename), dir)
-    });
+  .command("serve [filename]")
+  .description("Open a file for editing")
+  .option("-p, --port <number>", "port to run server on", "4005")
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    const isLocalApiError = (err: any): err is LocalApiError => {
+      return typeof err.code === "string";
+    };
+
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+    } catch (err) {
+      if (isLocalApiError(err)) {
+        if (err.code === "EADDRINUSE") {
+          console.error("Port is in use. Try running on a different port.");
+        }
+      } else if (err instanceof Error) {
+        console.log("Heres the problem", err.message);
+      }
+      process.exit(1);
+    }
+  });
